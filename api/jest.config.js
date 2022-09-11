dea76ac26610e6e206ef8ccc632b0fa621209a2f
Romanize <robert.g.martinez.a@gmail.com>
2022-09-11 03:51:52 -0300

    eslint config

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,9 +1,7 @@
-const jestConfig = {
+module.exports = {
   // Sync with ./tsconfig.json paths
   moduleNameMapper: {
-    '^@src/(.*)': '<rootDir>/src/$1',
+    '^~/(.*)': '<rootDir>/src/$1',
     '^@controllers/(.*)': '<rootDir>/src/controllers/$1',
-  }
+  },
 };
-
-export default jestConfig;
