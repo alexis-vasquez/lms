f8b99f6f3c53aa7b8408a7eb9668482fd84f66c4
alexis-vasquez <alexisjvr08@gmail.com>
2022-10-20 00:11:55 -0300

    chore(Api): :construction: Add Category Model

---
+++ apps/api/src/database/models/Category.ts
@@ -0,0 +1,29 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '..';
+
+interface CategoryModel extends Model {
+  id: number;
+  name: string;
+}
+
+export const Category = sequelize.define<CategoryModel>(
+  'Category',
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+      },
+  },
+  {
+    timestamps: false,
+  }
+);
