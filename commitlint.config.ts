e1dc67250796769881b6f300e71655d22963efe6
Romanize <robert.g.martinez.a@gmail.com>
2022-10-21 02:09:09 -0300

    chore: :sparkles: add husky config for commits

---
+++ commitlint.config.ts
@@ -0,0 +1,29 @@
+import type { UserConfig } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ["@commitlint/config-conventional"],
+  /*
+   * Resolve and load @commitlint/format from node_modules.
+   * Referenced package must be installed
+   */
+  formatter: "@commitlint/format",
+  /*
+   * Functions that return true if commitlint should ignore the given message.
+   */
+  ignores: [(commit: string) => commit === ""],
+  /*
+   * Whether commitlint uses the default ignore rules.
+   */
+  defaultIgnores: true,
+  /*
+   * Custom URL to show upon failure
+   */
+  helpUrl:
+    "https://github.com/conventional-changelog/commitlint/#what-is-commitlint",
+};
+
+export default Configuration;
